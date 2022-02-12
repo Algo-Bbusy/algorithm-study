231477e395e086604b13c27c76942bc8ca58a0c4
jeonyuneo <piq2255@gmail.com>
2022-02-12 10:57:09 +0900

    BOJ_2116_주사위쌓기_어정윤 - 시간초과

---
+++ day2/BOJ_2116_주사위쌓기/BOJ_2116_주사위쌓기_어정윤.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_2116_주사위쌓기_어정윤 {
+
+    private static List<int[]> dices;
+    private static int[][] sides;
+    private static int under;
+    private static int max;
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+        n = Integer.parseInt(bufferedReader.readLine());
+        dices = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            int c = Integer.parseInt(stringTokenizer.nextToken());
+            int d = Integer.parseInt(stringTokenizer.nextToken());
+            int e = Integer.parseInt(stringTokenizer.nextToken());
+            int f = Integer.parseInt(stringTokenizer.nextToken());
+            dices.add(new int[]{a, f, b, d, c, e});
+        }
+
+        sides = new int[n][4];
+        int underIdx = 0;
+        for (int i = 0;i < 6;i++) {
+            for (int j = 0; j < n; j++) {
+                int cnt = 0;
+                if (underIdx % 2 == 0) {
+                    under = dices.get(j)[++underIdx];
+                } else {
+                    under = dices.get(j)[--underIdx];
+                }
+
+                for (int k = 0; k < 6; k++) {
+                    int overIdx;
+                    if (underIdx % 2 == 0) {
+                        overIdx = underIdx + 1;
+                    } else {
+                        overIdx = underIdx - 1;
+                    }
+                    if (k != underIdx && k != overIdx) {
+                        sides[j][cnt++] = dices.get(j)[k];
+                    }
+                }
+
+                if (j < n-1) {
+                    for (int k = 0; k < 6; k++) {
+                        if (dices.get(j+1)[k] == under) {
+                            underIdx = k;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            repeatedPermutation(0, 0);
+        }
+        System.out.println(max);
+    }
+
+    private static void repeatedPermutation(int cnt, int sum) {
+        if (cnt == n) {
+            max = Math.max(max, sum);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            repeatedPermutation(cnt+1, sum+sides[cnt][i]);
+        }
+    }
+}
