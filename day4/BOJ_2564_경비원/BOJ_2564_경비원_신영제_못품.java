caae0f4e3a5239f9564c227a67d3f59b352b01fe
신영제 <zerojei0423@naver.com>
2022-02-20 04:03:51 +0900

    BOJ_2564_경비원_신영제_게으름으로 인하여 풀지 못했습니다..

---
+++ day4/BOJ_2564_경비원/BOJ_2564_경비원_신영제_못품.java
@@ -0,0 +1,13 @@
+package com.ssafy.study.homework4;
+
+public class BOJ_2564 { // 경비원
+
+	public static void main(String[] args) {
+		/*
+		 * 동근이와 상점 사이의 상대 거리 구해보기
+		 * 동근이 위치가 동서남북일때 상점과 최소거리를 따로 구하면 될거같은데..
+		 * 저의 게으름으로 인하여 풀지 못하였던거 같습니다..
+		 */
+	}
+
+}
