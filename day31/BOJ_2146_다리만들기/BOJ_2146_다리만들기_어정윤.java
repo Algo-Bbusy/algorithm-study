55cbd7fe7e44374ce39353fe94e5ba332bf69a85
jeonyuneo <piq2255@gmail.com>
2022-07-24 17:44:01 +0900

    BOJ_2146_다리만들기_어정윤

--- day31/BOJ_2146_다리만들기/BOJ_2146_다리만들기_어정윤.java
+++ day31/BOJ_2146_다리만들기/BOJ_2146_다리만들기_어정윤.java
@@ -25,26 +25,28 @@ public class BOJ_2146_다리만들기_어정윤 {
                 }
             }
         }
-        int[][] land = getLand(isLand, n);
-        findMinBridge(land, n);
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] land = getLand(isLand, queue, n);
+        findMinBridge(land, queue, n);
         System.out.println(minBridge);
     }
 
-    private static void findMinBridge(int[][] land, int n) {
+    private static void findMinBridge(int[][] land, Queue<int[]> queue, int n) {
         minBridge = n * n;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (land[i][j] != EMPTY) {
-                    buildBridge(land, n, i, j);
+                    buildBridge(land, queue, n, i, j);
                 }
             }
         }
     }
 
-    private static void buildBridge(int[][] land, int n, int i, int j) {
+    private static void buildBridge(int[][] land, Queue<int[]> queue, int n, int i, int j) {
+        boolean[][] isSelected = new boolean[n][n];
+        isSelected[i][j] = true;
         int fromLand = land[i][j];
         int bridge = 0;
-        Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{i, j});
         while (!queue.isEmpty()) {
             int size = queue.size();
@@ -55,7 +57,8 @@ public class BOJ_2146_다리만들기_어정윤 {
                 for (int[] delta : DELTAS) {
                     int dx = x + delta[0];
                     int dy = y + delta[1];
-                    if (dx >= 0 && dx < n && dy >= 0 && dy < n) {
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < n && !isSelected[dx][dy] && land[dx][dy] != fromLand) {
+                        isSelected[dx][dy] = true;
                         if (land[dx][dy] == EMPTY) {
                             queue.offer(new int[]{dx, dy});
                         } else if (land[dx][dy] != fromLand) {
@@ -70,11 +73,10 @@ public class BOJ_2146_다리만들기_어정윤 {
         }
     }
 
-    private static int[][] getLand(boolean[][] isLand, int n) {
+    private static int[][] getLand(boolean[][] isLand, Queue<int[]> queue, int n) {
         int landNumber = 1;
         int[][] land = new int[n][n];
         boolean[][] isVisited = new boolean[n][n];
-        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (isLand[i][j] && !isVisited[i][j]) {
